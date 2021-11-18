3de3ad9d6416d304d1af34bacb87c477e4cbda9c
Amit Agarwal <amagarw@microsoft.com>
2021-11-18 14:02:37 +0530

    Day 1 - Added HTML

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { UsersGridComponent } from './users-grid/users-grid.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    NavbarComponent,
+    UsersGridComponent
   ],
   imports: [
     BrowserModule
